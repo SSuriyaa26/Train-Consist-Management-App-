7fd664d4111a164cc8408ff78b74236898401c21
SSuriyaa26 <ssuriyaa26@gmail.com>
2026-04-01 14:55:42 +0530

    UC2:Hardcoded palindrome

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -1,17 +1,34 @@
 import java.util.*;
 public class TrainConsistManagement {
     public static void main(String[] args) {
+        List<String> passengerBoots = new ArrayList<>();
 
-        System.out.println("======================================");
-        System.out.println("=== Train Consist Management App ===");
+
+
+        System.out.println("\n======================================");
+        System.out.println("UC2 - Add Passenger Bogies to Train");
         System.out.println("======================================\n");
 
-        List<String> trainConsist = new ArrayList<>();
+        passengerBoots.add("Sleeper");
+        passengerBoots.add("AC Chair");
+        passengerBoots.add("First Class");
 
-        System.out.println("Train initialized successfully...");
-        System.out.println("Initial bogie count: " + trainConsist.size());
-        System.out.println("Current Train Consist: " + trainConsist);
-        System.out.println("\nSystem ready for operations...");
+        System.out.println("After Adding Bogies:");
+        System.out.println("Passenger Bogies : " + passengerBoots);
+
+        passengerBoots.remove("AC Chair");
+
+        System.out.println("\nAfter Removing 'AC Chair':");
+        System.out.println("Passenger Bogies : " + passengerBoots);
+
+        System.out.println("\nChecking if 'Sleeper' exists:");
+        System.out.println("Contains Sleeper? : " + passengerBoots.contains("Sleeper"));
+
+        System.out.println("\nFinal Train Passenger Consist:");
+        System.out.println(passengerBoots);
+
+        System.out.println("\nUC2 operations completed successfully...");
+
 
     }
 }
